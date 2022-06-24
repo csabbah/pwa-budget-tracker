63b280f705c9ce80b61f3199628a5d9a3239bdf1
Csabbah <csabbah@ryerson.ca>
2022-06-24 16:02:33 -0400

    Setup Activate phase for the Service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,3 +18,29 @@ self.addEventListener('install', function (e) {
     })
   );
 });
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    // Return an array of the cache names (keyList)
+    caches.keys().then(function (keyList) {
+      // Save them to an array called cacheKeeplist whil also applying a filter method to...
+      let cacheKeeplist = keyList.filter(function (key) {
+        // ...return the caches that have app prefix
+        return key.indexOf(APP_PREFIX);
+      });
+
+      // Add the current cache to the keepList by pushing it
+      cacheKeeplist.push(CACHE_NAME);
+
+      // Return a Promise that resolves once all old version of the cache have been deleted
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('Deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
