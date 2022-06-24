088e9df7e5c6587bf822fef2aeb2e0cf41edbe96
Csabbah <csabbah@ryerson.ca>
2022-06-24 16:19:00 -0400

    Setup Fetch phase for the Service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -44,3 +44,25 @@ self.addEventListener('activate', function (e) {
     })
   );
 });
+
+// Listen for the fetch event, log the URL of the requested resource then...
+// begin to define how we will response wto the request
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url);
+  // The lines inside responseWith will deliver the resources directly from the cache, otherwise...
+  // The resources will be retrieved normally
+  e.respondWith(
+    // We use .match() to check if the resources already exists in caches
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        // If resources exist, log the URL with a message then return the cached resources
+        console.log('responding with cache : ' + e.request.url);
+        return request;
+      } else {
+        // If resources do not exist in cache, allow th resource to be retrieved from the online network as usual
+        console.log('file is not cached, fetching : ' + e.request.url);
+        return fetch(e.request);
+      }
+    })
+  );
+});
